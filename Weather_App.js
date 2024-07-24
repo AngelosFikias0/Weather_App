2d0bb907f92734b589949835b2e46253053d6479
Angelos Fikias <angelosfikias@gmail.com>
2024-07-24 14:11:23 +0300

    Update Weather_App.js

--- Weather_App.js
+++ Weather_App.js
@@ -1,4 +1,4 @@
-const apiKey = //'Your api key here';
+const apiKey = /*'Your api key here';*/
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const locationInput = document.getElementById('locationInput');
